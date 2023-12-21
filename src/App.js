de55286e3795e4da2441eeb32466c35bbd2474e8
popelkin-roman <mymaythe23@gmail.com>
2023-12-21 23:46:24 +0500

    Working project withot styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,71 @@
+import { marked } from "marked";
+import { useState } from "react";
+
+marked.use({
+  breaks: true,
+});
+
 function App() {
-  return <div className="App"></div>;
+  let defaultText = `# Welcome to my React Markdown Previewer
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  `;
+  const parseText = (txt) => {
+    return marked.parse(txt);
+  };
+  let parsedText = parseText(defaultText);
+  let [editorText, setEditorText] = useState(parsedText);
+  const theObj = { __html: editorText };
+
+  return (
+    <div className="App">
+      <textarea
+        id="editor"
+        onChange={(e) => setEditorText(parseText(e.target.value))}
+        defaultValue={defaultText}
+      ></textarea>
+      <div id="preview" dangerouslySetInnerHTML={theObj}></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/window/Window.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Window = (title) => {
+  <header>{title}</header>;
+};
+
+export default Window;
